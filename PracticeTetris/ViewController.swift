c537d5837d21df43cb8318bbe517f2db1e3c1a98
Xavier Lian <xavierliancw@gmail.com>
2018-10-15 18:41:34 -0700

    Initial Commit

---
+++ PracticeTetris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeTetris
+//
+//  Created by Xavier Lian on 10/15/18.
+//  Copyright © 2018 Xavier Lian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
